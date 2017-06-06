6c7862279fa28147afe9737f2a461fd2fca1a142
Sagar Chamling <sgr.raee@gmail.com>
2017-06-06 17:33:19 +0545

    Selecting schema test in route

--- routes/web.php
+++ routes/web.php
@@ -11,12 +11,36 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+use Illuminate\Support\Facades\DB;
+
+Route::get(
+    '/',
+    function () {
+        return view('welcome');
+    }
+);
 
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
 Route::resource('/tenants', 'TenantsController');
+
+Route::get(
+    'apple',
+    function () {
+        $result = DB::statement('SET search_path TO apple');
+        $query  = DB::select('show search_path');
+
+        return array_pop($query)->search_path;
+    }
+);
+
+Route::get(
+    'ball',
+    function () {
+        $query = DB::select('show search_path');
+
+        return array_pop($query)->search_path;
+    }
+);
