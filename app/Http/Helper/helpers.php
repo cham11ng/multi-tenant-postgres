accb6eaf7263436c351b07c57f33e4bbcfca57bf
Sagar Chamling <sgr.raee@gmail.com>
2017-06-13 13:24:53 +0545

    More generic tenant_route helper class

--- app/Http/Helper/helpers.php
+++ app/Http/Helper/helpers.php
@@ -3,8 +3,10 @@
 use Illuminate\Support\Facades\Request;
 
 if (!function_exists('tenant_route')) {
-    function tenant_route($route)
+    function tenant_route($route, $parameters = [], $absolute = true)
     {
-        return route($route, ['tenant' => Request::route()->parameter('tenant')]);
+        $tenant_parameter = ['tenant' => Request::route()->parameter('tenant')];
+        $parameters = $parameters ? array_merge($tenant_parameter, $parameters) : $tenant_parameter;
+        return route($route, $parameters, $absolute);
     }
 }
