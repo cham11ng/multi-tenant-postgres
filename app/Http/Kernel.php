d09b09d8a2e9dc5205b91946e64586aabe5ebe83
Sagar Chamling <sgr.raee@gmail.com>
2017-06-13 10:51:51 +0545

    Ordering middleware at top

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -50,12 +50,12 @@ class Kernel extends HttpKernel
      * @var array
      */
     protected $routeMiddleware = [
+        'select-schema' => \App\Http\Middleware\SwitchSchema::class,
         'auth' => \Illuminate\Auth\Middleware\Authenticate::class,
         'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
         'bindings' => \Illuminate\Routing\Middleware\SubstituteBindings::class,
         'can' => \Illuminate\Auth\Middleware\Authorize::class,
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
-        'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
-        'select-schema' => \App\Http\Middleware\SwitchSchema::class
+        'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class
     ];
 }
