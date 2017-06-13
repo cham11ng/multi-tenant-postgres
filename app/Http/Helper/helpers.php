e3f254f84ef35fbbc44df766366de402423d4145
Sagar Chamling <sgr.raee@gmail.com>
2017-06-13 13:39:32 +0545

    Add PHPDoc Blocks for helper function

--- app/Http/Helper/helpers.php
+++ app/Http/Helper/helpers.php
@@ -3,6 +3,14 @@
 use Illuminate\Support\Facades\Request;
 
 if (!function_exists('tenant_route')) {
+    /**
+     * Tenant route is for Dynamic sub-domain routing
+     *
+     * @param       $route
+     * @param array $parameters
+     * @param bool  $absolute
+     * @return string
+     */
     function tenant_route($route, $parameters = [], $absolute = true)
     {
         $tenant_parameter = ['tenant' => Request::route()->parameter('tenant')];
