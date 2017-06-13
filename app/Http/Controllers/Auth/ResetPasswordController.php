ea8b55114a04ca94cbb4baf6b12bfa6736c53586
Sagar Chamling <sgr.raee@gmail.com>
2017-06-13 13:52:20 +0545

    Fix Reset Password

--- app/Http/Controllers/Auth/ResetPasswordController.php
+++ app/Http/Controllers/Auth/ResetPasswordController.php
@@ -38,6 +38,22 @@ class ResetPasswordController extends Controller
     }
 
     /**
+     * Display the password reset view for the given token.
+     *
+     * If no token is present, display the link request form.
+     *
+     * @param \Illuminate\Http\Request|Request $request
+     * @param  string|null                     $token
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function showResetForm(Request $request, $tenant, $token = null)
+    {
+        return view('auth.passwords.reset')->with(
+            ['token' => $token]
+        );
+    }
+
+    /**
      * Redirect after resetting password
      *
      * @return string
